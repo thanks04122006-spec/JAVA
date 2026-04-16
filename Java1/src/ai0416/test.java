261b07d1a906f6f7340ae82476309825de244aae
thanks04122006-spec <thanks04122006@gmail.com>
2026-04-16 11:45:15 +0900

    Changes

--- Java1/src/ai0416/test.java
+++ Java1/src/ai0416/test.java
@@ -2,6 +2,14 @@ package ai0416;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("sss");
+        int num = 200;
+        if (num < 100){
+            System.out.println("100보다 작");
+        }
+        else{
+            System.out.println("100보다 크");
+        }
+
+        System.out.println("프로그램 출력 끝");
     }
 }
