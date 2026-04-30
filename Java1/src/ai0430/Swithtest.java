38777c206400afbe1a1fb2ff6152e88083881ee6
thanks04122006-spec <thanks04122006@gmail.com>
2026-04-30 09:48:54 +0900

    0430 1교시

---
+++ Java1/src/ai0430/Swithtest.java
@@ -0,0 +1,34 @@
+package ai0430;
+
+import java.util.Scanner;
+
+public class Swithtest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("1 ~ 10까지 중에서 정수를 입력: ");
+        int num = s.nextInt();
+        String result = "";
+
+        switch (num)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+                result = "홀수";
+                break;
+
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+                result = "짝수";
+                break;
+        }
+        System.out.printf("입력된 숫자 %d는(은) %s입니다\n", num, result);
+
+        s.close();
+    }
+}
