23aa069ca8a28fbf5a2402e0ee190ffc8f65b169
thanks04122006-spec <thanks04122006@gmail.com>
2026-08-18 15:00:55 +0900

    0818 1차원 배열

---
+++ Java1/src/Array/ArratTest2.java
@@ -0,0 +1,28 @@
+﻿package Array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArratTest2 {
+    public static void main(String[] args) {
+        String[] subject = {"프로그래밍언어실습","데이터베이스","융합 UI실습"};
+
+        System.out.println(" 현재 배열의 길이 : "+ subject.length);
+        System.out.println(" 현재 배열의 내용: ");
+
+        for(String subjects : subject){
+            System.out.println(subject + " ");
+        }
+        System.out.println();
+
+        subject = Arrays.copyOf(subject , subject.length + 2);
+        subject[subject.length-2] = "인공지능처리";
+        subject[subject.length-1] = "직업과 경력개발";
+
+        System.out.println("새로운 배열의 길이 "+ subject.length);
+        for(String subjects : subject){
+            System.out.println(subject + " ");
+        }
+        System.out.println();
+    }
+}
